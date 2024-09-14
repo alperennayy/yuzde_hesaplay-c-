2ee6eecee662c7ff23afc63437b43e23ae4ae53b
alperennayy <154600553+alperennayy@users.noreply.github.com>
2024-09-14 19:32:03 +0300

    yuzde hesaplayici
    its basic project but i learned something as well

---
+++ yüzde_hesaplayıcı/untitled11.py
@@ -0,0 +1,7 @@
+
+number = int(input("Which number's percentage would you like to calculate: "))
+x = int(input(f"What percentage of {number} would you like to calculate: "))
+result = number * (x / 100)
+
+print(f"{x}% of {number} is: {result}")
+
